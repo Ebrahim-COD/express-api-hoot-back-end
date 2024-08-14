e0d495239272418973c8d4dda0f3135d9f3c7d9b
EBRAHIM-CODE <ebrahim0ramadhan@gmail.com>
2024-08-14 10:16:21 +0300

    added hook model

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,29 @@
-const express = require('express');
+const express = require("express");
 // auth
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Models
-const User = require('../models/user');
+const User = require("../models/user");
 
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const { username, password } = req.body;
 
     const existingUser = await User.findOne({ username });
 
     if (existingUser) {
-      return res.status(400).json({ error: 'Something went wrongm, try again.' });
+      return res
+        .status(400)
+        .json({ error: "Something went wrongm, try again." });
     }
 
-    const hashedPassword = bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS));
+    const hashedPassword = bcrypt.hashSync(
+      password,
+      parseInt(process.env.SALT_ROUNDS)
+    );
 
     const user = await User.create({ username, hashedPassword });
 
@@ -32,24 +37,27 @@ router.post('/signup', async (req, res) => {
 
     return res.status(201).json({ user, token });
   } catch (error) {
-    res.status(400).json({ error: 'Something wen wrong, try again.' });
+    res.status(400).json({ error: "Something wen wrong, try again." });
   }
 });
 
-router.post('/signin', async (req, res) => {
+router.post("/signin", async (req, res) => {
   try {
     const { username, password } = req.body;
 
     const existingUser = await User.findOne({ username });
 
     if (!existingUser) {
-      return res.status(400).json({ error: 'Invalid Credentials' });
+      return res.status(400).json({ error: "Invalid Credentials" });
     }
 
-    const isValidPassword = bcrypt.compareSync(password, existingUser.hashedPassword);
+    const isValidPassword = bcrypt.compareSync(
+      password,
+      existingUser.hashedPassword
+    );
 
     if (!isValidPassword) {
-      throw Error('Invalid Credentials');
+      throw Error("Invalid Credentials");
     }
 
     const token = jwt.sign(
@@ -62,8 +70,10 @@ router.post('/signin', async (req, res) => {
 
     return res.status(200).json({ user: existingUser, token });
   } catch (error) {
-    res.status(400).json({ error: 'Something went wrong, try again.' });
+    res.status(400).json({ error: "Something went wrong, try again." });
   }
 });
 
 module.exports = router;
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2YmM1NTk5YWE3OGQ5ZjY2NjUwMDViZCIsInVzZXJuYW1lIjoiRWJyYWhpbSIsImlhdCI6MTcyMzYxODg3N30.kxiqDILtqJ-Fmmb-h5Wnd3xnXcw2xCpxvzVqpyfngck
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const usersRouter = require('./controllers/users');
 const profilesRouter = require('./controllers/profiles');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(morgan('dev'));
